f643907468ecbd8dd99cd5d67dfeafbb2f6fe964
Shourav kumar <157200350+shourav121@users.noreply.github.com>
2025-10-25 01:37:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://five-onecart-frontendseven.onrender.com" , "http://localhost:5174"],
+ origin:["https://five-onecart-frontendseven.onrender.com" , "https://five-onecartadmin.onrender.com"],
  credentials:true
 }))
 
