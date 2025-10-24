501ded48821e525b5a2a8d39810475855ef091b4
Shourav kumar <157200350+shourav121@users.noreply.github.com>
2025-10-25 00:55:46 +0530

    Update server URL to production endpoint

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:8000"
+    let serverUrl = "https://five-onecart-backend-w29c.onrender.com"
 
     let value = {
        serverUrl
